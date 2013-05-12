a2cd582de5cf0e4ba08a5fee8f1903cebc95f978
chenhui <vagrant@precise32.(none)>
2013-05-12 02:56:38 +0000

    Finished web template

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,16 +1,24 @@
 require 'spec_helper'
 
 describe "Pages" do
+
+  let(:base_title){"BingWen"}
+
   describe "Home page" do
     it "should have the content 'sample App'" do
     	visit '/pages/home'
     	page.should  have_selector('h1',:text=>'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/pages/home'
       page.should have_selector('title',
-        :text=>"BingWen | Home")
+        :text=>"#{base_title}")
+    end
+
+    it "should not have a custome page title" do
+      visit '/pages/home'
+      page.should_not have_selector('title',:text=>'| Home')
     end
   end
 
@@ -24,7 +32,7 @@ describe "Pages" do
     it "should have the title 'Contact'" do
       visit '/pages/contact'
       page.should have_selector('title',
-        :text=>"BingWen | Contact")
+        :text=>"#{base_title} | Contact")
     end
   end
 
@@ -34,11 +42,10 @@ describe "Pages" do
   		page.should have_selector('h1',:text=>"About Us")
   	end
 
-
     it "should have the title 'About'" do
       visit '/pages/about'
       page.should have_selector('title',
-        :text=>"BingWen | About")
+        :text=>"#{base_title} | About")
     end
   end
 end
